2c9bdebe8a5a2b9d7a320535e53a02e6dbf0252e
Fengxu_W <wangfengxu@outlook.com>
2018-10-07 21:43:35 +0800

    新建用户表，并在admin中注册

--- webapp/admin.py
+++ webapp/admin.py
@@ -2,5 +2,6 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from webapp.models import *
 # Register your models here.
+admin.site.register(User)
--- webapp/models.py
+++ webapp/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    account = models.CharField(max_length=30)
+    passwd = models.CharField(max_length=30)
